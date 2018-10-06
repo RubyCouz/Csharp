014c93d0c6bc896c7c1dec2e75b69cc60281d63b
Cédric <cousin.ruby.cedric@gmail.com>
2018-10-06 18:22:37 +0200

    début partie 2, exo 1, 2 et 3

---
+++ part2/exo1/exo1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 15;
+            if (age >= 18)
+                Console.WriteLine("Vous êtes majeur!!");
+            else
+                Console.WriteLine("Vous êtes mineur!!");
+        }
+    }
+}
---
+++ part2/exo2/exo2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isEasy = true;
+            if (isEasy == true)
+                Console.WriteLine("C'est facile!!");
+            else
+                Console.WriteLine("C'est difficile!!");
+        }
+    }
+}

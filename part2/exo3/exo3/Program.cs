062afab5d8bee23222b37cdd21391f32b01ba778
Cédric <cousin.ruby.cedric@gmail.com>
2018-10-07 16:48:39 +0200

    exo3 refait

--- part2/exo3/exo3/Program.cs
+++ part2/exo3/exo3/Program.cs
@@ -6,7 +6,17 @@ namespace exo3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int age = 34;
+            string gender = "homme";
+            if (age >= 18 && gender == "homme")
+                Console.WriteLine("Vous êtes un homme et vous êtes majeur.");
+            else if (age >= 18 && gender == "femme")
+                Console.WriteLine("Vous êtes majeur et vous êtes une femme.");
+            else if (age <= 18 && gender == "femme")
+                Console.WriteLine("Vous êtes mineur et vous êtes une femme.");
+            else if (age <= 18 && gender == "homme")
+                Console.WriteLine("Vous êtes mineur et vous êtes une homme.");
+
         }
     }
 }
